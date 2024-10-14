9c3eb7e07627cc77ba3804f4a341afd03b8540f4
Staunch <staunch@Staunchs-MacBook-Pro.local>
2024-10-14 11:15:35 +0500

    Escape characters

---
+++ 02 - Getting Started/escape_characters.rb
@@ -0,0 +1,7 @@
+puts "Hello my name is haris \nI am a software Engineer"
+puts"\t I work in staunch"
+puts "Hello \"Good Morning\""
+puts 'Hello \'Good Night\''
+puts 'Hello "Good Night"'
+puts "Hello 'Good Morning'"
+#we can use double qoutes in single code and single qoute in double qoutes but to use the same we need escape characters#
