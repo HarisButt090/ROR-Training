012448d77ce5833017a3c01de1a0281666c5ef64
Staunch <staunch@Staunchs-MacBook-Pro.local>
2024-10-14 11:57:58 +0500

    Basic Arithmetic in ruby

---
+++ 02 - Getting Started/basic_arithmetic_in_ruby.rb
@@ -0,0 +1,19 @@
+puts 1+5
+puts 6-7
+puts 6*7
+puts 15/3
+
+puts 15/4
+puts 15.0/4
+puts 15/4.0
+puts 15.0/4.0
+
+puts 0.5/2
+
+
+puts 5 + 4 * 6 / 1 #PEMDAS Parenthesis,Exponents, Multiplication,division, Addition, subtraction Rubyy folloows the PEMDAS rule to solve multiple arithmetic operation in one 
+
+
+puts 10%2
+puts 56%20
+
