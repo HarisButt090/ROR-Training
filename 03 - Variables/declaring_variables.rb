49fd410fabb6f9ff1c3936181e0be8a23bf9eccf
Staunch <staunch@Staunchs-MacBook-Pro.local>
2024-10-14 12:46:57 +0500

    Declaring variables and exerciss

---
+++ 03 - Variables/declaring_variables.rb
@@ -0,0 +1,58 @@
+# variable start woith lower case or underscore
+# variable should have letters, numbers, underscores
+# variables should not have spaces
+# we can also snake-case formatting in bigger variables like my_favorite_fruit
+# there are 40+ reserved keyword in ruby, we should not use them as variable names like print, puts,
+
+
+age=45
+puts age
+puts age+3
+
+f_name="Haris"
+l_name="Butt"
+puts f_name
+puts l_name
+puts f_name + l_name
+
+age = 35
+puts age 
+puts age + 5
+age=age + 5
+puts age
+
+
+#Exwercises
+#Variable Declaration
+#Declare a variable age and assign it your age. Print the value of age.
+
+age=22
+puts age
+
+#Multiple Variables
+#Create three variables: height, weight, and gender. Assign them values and print them in a sentence using string interpolation.
+height=5.10
+weight=70
+gender="male"
+puts "My height is #{height} and my weight is #{weight} and my gender is #{gender}"
+
+
+
+#Reassignment of Variables
+#Declare a variable temperature and assign it a value. Then, reassign temperature with a different value and print both before and after reassignment.
+temperature=50
+puts temperature
+temperature=60
+puts temperature
+
+#Coding exercise 
+# Declare a season variable and assign it a string of "Fall".
+# Output the value of the season variable to the screen.
+
+season="Fall"
+puts season
+
+# Declare a hard_math variable and assign it the sum of 3, 4 and 5.
+# Output the value of the hard_math variable to the screen.
+hard_math=3+4+5
+puts hard_math
