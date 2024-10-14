9bd0acddeb15bc27266ce6340bc68f7b099a2442
Staunch <staunch@Staunchs-MacBook-Pro.local>
2024-10-14 14:32:16 +0500

    SOme more concepts

---
+++ 03 - Variables/constants.rb
@@ -0,0 +1,5 @@
+pi=3.1416
+puts pi
+PI=3.14
+puts PI
+#main differnece is that ee have to keep the variable name in capslock if we want to create a constant ootherwise just declare a normal variable
