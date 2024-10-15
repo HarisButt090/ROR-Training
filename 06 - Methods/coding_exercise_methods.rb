b0358f16e08a0ca3e1e302f9a5fbebbb3f28fdba
Staunch <staunch@Staunchs-MacBook-Pro.local>
2024-10-15 11:27:47 +0500

    Methods, arguments Exercises

---
+++ 06 - Methods/coding_exercise_methods.rb
@@ -0,0 +1,34 @@
+=begin
+# Define a easy_money method that accepts no parameters 
+# and always returns the value 100.
+
+
+# Define a best_food_ever method that accepts 
+# no parameters and always returns the string "Sushi".
+
+
+# Define a convert_to_currency method that accepts a single parameter (an integer). The method should convert the argument to a string, prefix it with a dollar sign, and return the result.
+# 
+# Examples:
+# The => indicates the expected return value
+# convert_to_currency(15)    => "$15"
+# convert_to_currency(8)     => "$8"
+
+=end
+def easy_money()
+  return 100
+end
+value=easy_money
+puts value
+
+def best_food_ever
+  return "String"
+end
+p best_food_ever
+
+def convert_to_currency(value)
+  return "$#{value}"
+end
+
+result=convert_to_currency(15)
+puts result
