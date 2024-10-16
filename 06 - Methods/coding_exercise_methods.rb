865c518358c44963d191c2fd0b71ab6d954539bd
HarisButt090 <haris@staunch.co>
2024-10-16 18:49:04 +0500

    Update coding_exercise_methods.rb

--- 06 - Methods/coding_exercise_methods.rb
+++ 06 - Methods/coding_exercise_methods.rb
@@ -1,12 +1,25 @@
 =begin
 # Define a easy_money method that accepts no parameters 
 # and always returns the value 100.
+=end
 
+def easy_money()
+  return 100
+end
+value = easy_money
+puts value # this will take print the return value from function
 
+=begin
 # Define a best_food_ever method that accepts 
 # no parameters and always returns the string "Sushi".
+=end
 
+def best_food_ever
+  return "String"
+end
+p best_food_ever
 
+=begin
 # Define a convert_to_currency method that accepts a single parameter (an integer). The method should convert the argument to a string, prefix it with a dollar sign, and return the result.
 # 
 # Examples:
@@ -15,20 +28,10 @@
 # convert_to_currency(8)     => "$8"
 
 =end
-def easy_money()
-  return 100
-end
-value=easy_money
-puts value
-
-def best_food_ever
-  return "String"
-end
-p best_food_ever
 
 def convert_to_currency(value)
   return "$#{value}"
 end
 
 result=convert_to_currency(15)
-puts result
+puts result # this will print the argument with currency symbol
