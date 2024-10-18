0d687a2adbf34a46697a66c9ea08ca28db2f6fba
HarisButt090 <haris@staunch.co>
2024-10-18 10:26:43 +0500

    Update the_next_keyword.rb

--- 10 - Loops/the_next_keyword.rb
+++ 10 - Loops/the_next_keyword.rb
@@ -1,7 +1,7 @@
 # This function finds all occurrences of the '$' sign in the given sentence using a simple while loop.
 # It prints the index of each '$' found in the string.
 
-def find_dollars_while(money_sentence)
+def find_dollars_sign_indices(money_sentence)
   current_index = 0 
   final_index = money_sentence.length - 1 
   
@@ -18,13 +18,13 @@ end
 
 money_sentence = "I love $ in the morning, $ in the afternoon, and $ at night"
 
-puts "Find Dollar method result using while loop"
-find_dollars_while(money_sentence)
+puts "Finding dollar sign ( using while lopp ) "
+find_dollars_sign_indices(money_sentence)
 
 
 # This function finds all the '$' sign in the given sentence using the `next` keyword.
 
-def find_dollars_next(money_sentence)
+def find_dollars_signs_indices(money_sentence)
   current_index = 0 
   final_index = money_sentence.length - 1 
 
@@ -41,5 +41,5 @@ def find_dollars_next(money_sentence)
   # The loop will terminate once all characters have been checked.
 end
 
-puts "Find Dollar method result using next keyword"
-find_dollars_next(money_sentence)
+puts "Finding dollar sign ( using while loop and next keyword ) "
+find_dollars_signs_indices(money_sentence)
