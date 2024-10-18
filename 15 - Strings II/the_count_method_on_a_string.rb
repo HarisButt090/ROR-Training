84c3d4ecfbbec925dfca604d716b18a250aa88f7
haris butt <haris@staunch.co>
2024-10-18 14:54:40 +0500

    Section 15 topics practiced

---
+++ 15 - Strings II/the_count_method_on_a_string.rb
@@ -0,0 +1,6 @@
+#count method count the number of occurrences of character(s) in a string 
+puts "Hello World".count("l") 
+puts "Hello World".count("w") 
+ puts "Hello World".count("lo") 
+  puts "Hello World".count("ol") 
+   puts "Hello World".count("W")
