73113034aa2367913932c91d201873f5e4d59d51
haris butt <haris@staunch.co>
2024-10-23 16:04:21 +0500

    Section 26 concepts (SETS) practiced

---
+++ 26 - Sets/add_and_delete_items_from_set.rb
@@ -0,0 +1,8 @@
+# The add method adds an element to the set
+# The delete method removes an element from the set 
+# Both methods return the Set itself
+sets=Set.new(["Summer","Winter","Fall","Winter","Spring"])
+p sets
+p sets.length
+p sets.add("Autum")
+p sets.delete("Fall")
