757e0b4fc6a30cd1dd98ed3ee0e2ba42ee0bff7e
haris butt <haris@staunch.co>
2024-10-23 10:33:12 +0500

    Sections 24 concepts practiced

---
+++ 24 - Input and Output/load_a_ruby_file_with_load_method.rb
@@ -0,0 +1,10 @@
+puts "Welcome to the program "
+
+load("another_file.rb")  # load "./another_file.rb"
+
+some_method
+
+table=Table.new
+puts table.class
+
+puts "We have reached the end of program"
---
+++ 24 - Input and Output/write_to_a_text_file.rb
@@ -0,0 +1,8 @@
+
+my_novel= File.open("my_file.txt","a") 
+  my_novel.puts "New content is added to file "
+  my_novel.write "No line break here!"
+  my_novel.puts "Very good! "
+
+
+
