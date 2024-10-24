f57f089eedd71dff4c6c1f0e2b201a071dfd9473
haris butt <haris@staunch.co>
2024-10-24 17:46:16 +0500

    Section 32 (GEMS) Last topic practiced

---
+++ 32 - Gems/use_faker_gem.rb
@@ -0,0 +1,13 @@
+require "faker"
+puts Faker.class
+puts Faker::Name.class
+puts Faker::Name.name
+puts Faker::Name.first_name
+puts Faker::Name.last_name
+# p Faker.constants.sort
+
+puts Faker::Music.instrument
+
+p Faker::Sports::Football.player
+
+puts Faker::WorldCup.name
